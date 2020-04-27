a2e0809b4c7622012e696ae3789b7e2d42c50d7e
kingsman142 <kingsman142@yahoo.com>
2020-04-27 18:40:16 -0400

    Finished Q6. Saving report

--- HW4/Q6/q6.m
+++ HW4/Q6/q6.m
@@ -1,20 +1,23 @@
 import brml.*;
 load("EMprinter.mat");
 
-EPS = 0.01;
-[N, K] = size(x, 2); % get number of samples
+fuse = array(1, condp(rand(1, 2)));
+drum = array(2, condp(rand(1, 2)));
+toner = array(3, condp(rand(1, 2))); 
+paper = array(4, condp(rand(1, 2)));
+roller = array(5, condp(rand(1, 2))); 
+burning = array([6 1], condp(rand(2, 2)));
+quality = array([7 2 3 4], condp(rand(2, 2, 2, 2)));
+wrinkled = array([8 1 4], condp(rand(2, 2, 2)));
+multpages = array([9 4 5], condp(rand(2, 2, 2))); 
+paperjam = array([10 1 5], condp(rand(2, 2, 2)));
 
-t = 1;
-tab = rand(2, 2);
-diff = 0.02;
-while diff > EPS % if true, we have not converged yet
-    t = t+1;
-    %for n = 1:N
-    %    % TODO: compute q_t^n % E step
-    %end
-    %for i = 1:K
-    %    
-    %end
-    
-    diff = norm(newTab - tab, 1); % L1 norm
-end
+% use EM algorithm to learn all CPTs of the network
+pot = {fuse, drum, toner, paper, roller, burning, quality, wrinkled, multpages, paperjam};
+[newpot, loglik] = EMbeliefnet(pot, x, {});
+
+% inference part of Q6
+joint = multpots(newpot);
+summedOut = sumpot(joint, [1 3 4 5 9 10]);
+drumUnitProbs = condp(squeeze(summedOut.table(:, 1, 1, 1)));
+fprintf("Prob. of drum unit problem: %.4f\n", drumUnitProbs(2));
